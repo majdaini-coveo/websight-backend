aea439e68594a1aeaa9ec50dbd13f896bbcc011f
majdaini <majdaini@coveo.com>
2025-06-12 13:18:47 -0400

    Adding a google oath token validation on the backend

---
+++ websight/googleOathVerify.js
@@ -0,0 +1,17 @@
+const {OAuth2Client} = require('google-auth-library');
+
+const client = new OAuth2Client();
+
+async function googleOathverification(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.WEB_CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  if (payload['hd'] !== 'coveo.com') {
+    throw new Error("Unauthorized user");
+  }
+}
+
+
+module.exports = googleOathverification
